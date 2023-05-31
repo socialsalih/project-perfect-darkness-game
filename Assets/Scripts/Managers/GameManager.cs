58d75e7c6777f7ab495386b744b2ffea86ff11cd
Burak Ensar Harmandar <88538924+BurakEnsarH@users.noreply.github.com>
2023-05-31 23:13:15 +0300

    Update GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public GameObject finishPanel;
+    public string playerTag = "Player";
+    public string finishTag = "Finish";
+
+    private bool gameIsFinished = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag(playerTag) && !gameIsFinished)
+        {
+            gameIsFinished = true;
+            StopGame();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StopGame()
     {
-        
+        // Disable player movement and control
+        player.GetComponent<PlayerController>().enabled = false;
+
+        // Open the finish panel
+        finishPanel.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        // Restart the level or perform any other action
+        // you want when the player clicks the restart button
+
+        // For example, you can reload the current scene
+        // using the following line:
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
