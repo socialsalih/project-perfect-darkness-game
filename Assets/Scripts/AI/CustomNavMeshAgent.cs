9381ed6d3c24e74da5c967d7c1d34c321cf2d3ea
Görkem Yasin <yasin_gul_2001@hotmail.com>
2023-06-08 15:29:00 +0300

    EnemyAI_Shooting

--- Assets/Scripts/AI/CustomNavMeshAgent.cs
+++ Assets/Scripts/AI/CustomNavMeshAgent.cs
@@ -6,6 +6,12 @@ using UnityEngine.AI;
 public class CustomNavMeshAgent : MonoBehaviour
 {
     [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private float timer = 5;
+    private float bulletTime;
+    public GameObject enemyBullet;
+    public Transform BulletSpawnPoint;
+    public float enemySpeed;
+
 
     private NavMeshAgent navMeshAgent;
 
@@ -16,6 +22,28 @@ public class CustomNavMeshAgent : MonoBehaviour
     private void Update()
     {
         navMeshAgent.destination = movePositionTransform.position;
+        ShootAtPlayer();
+    }
+
+    void ShootAtPlayer()
+    {
+        bulletTime -= Time.deltaTime;
+        if (bulletTime > 0) return;
+
+        bulletTime = timer;
+
+        GameObject bulletObj = Instantiate(enemyBullet, BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.rotation) as GameObject;
+        Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+        bulletRig.AddForce(bulletRig.transform.forward * enemySpeed);
+        Destroy(bulletObj, 5f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject bulletObj = Instantiate(enemyBullet, BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.rotation) as GameObject;
+            Destroy(bulletObj);
+        }
     }
 }
     
