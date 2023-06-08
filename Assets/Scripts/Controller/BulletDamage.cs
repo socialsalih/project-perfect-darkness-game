fdfa23805d9baa7c638dd633ed20ab01a6b5f743
Görkem Yasin <yasin_gul_2001@hotmail.com>
2023-06-08 22:32:22 +0300

    EnemyHealth

---
+++ Assets/Scripts/Controller/BulletDamage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+        }
+    }
+
+}
